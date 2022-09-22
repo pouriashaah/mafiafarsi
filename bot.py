19a3474bc16508e099c6ad73e0c898f8e91f5a2a
amirhoseyn saharkhiz <amirhoseyn.saharkhiz@gmail.com>
2022-09-22 23:52:58 +0330

    fixed 400 error bugg

--- bot.py
+++ bot.py
@@ -4,15 +4,17 @@ import responses
 from discord.ext import commands
 
 intents = discord.Intents.all()
-TOKEN = 'ljCS2P2lt6d-nbgDtnZy3vZmg'
-bot = commands.Bot(command_prefix = "!",intents=intents)
+TOKEN = 'MTAyMjI2MTM5NDg0MzkxMDE2NA.Gz3Spj.IpKGZjwVzRDxtMlMX-DiPsWYU08QN6fMlQXM2Y'
+bot = commands.Bot(command_prefix = "?",intents=intents)
 
 async def send_message(message, user_message, is_private):
-    try:
+
         response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+        if(response):
+            await message.author.send(response) if is_private else await message.channel.send(response)
+        else:
+            await message.author.send('Invalid request. Please refer to `!help` for aid.') if is_private else await message.channel.send('Invalid request. Please refer to `!help` for aid.')
+
 
 
 def run_discord_bot():
@@ -26,16 +28,18 @@ def run_discord_bot():
 
     @bot.event
     async def on_message(m):
+        bot_name = "amirtestingthis#9523"
         username = str(m.author)
         user_message = str(m.content)
         channel = str(m.channel)
         print(channel)
         # if user_message[0] == '?':
         #     user_message = user_message[1:]
-        if channel:
-            await send_message(m, user_message, is_private=False)
-        else:
-            await send_message(m, user_message, is_private=True)
+        if username!=bot_name:
+            if channel:
+                await send_message(m, user_message, is_private=False)
+            else:
+                await send_message(m, user_message, is_private=True)
     bot.run(TOKEN)
 
 
