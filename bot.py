f2d0a4a8a1f0cdb72653d5ae8eed635e4476ed30
amirhoseyn saharkhiz <amirhoseyn.saharkhiz@gmail.com>
2022-09-23 01:20:20 +0330

    bot now adds role text_chat and voice_chat on run if notavailable

--- bot.py
+++ bot.py
@@ -4,8 +4,12 @@ import responses
 from discord.ext import commands
 
 intents = discord.Intents.all()
-TOKEN = 'MTAyMjI2MTM5NDg0MzkxMDE2NA.GxOK3Y._U84DZuF2NHMyp-cZ3etEyYacFaHVejGuTwmhY'
+TOKEN = 'MTAyMjI2MTM5NDg0MzkxMDE2NA.G8u-C5.sdeczN0ctCSRG0DwsGGGquLPPrJOspvS9EUS28'
 bot = commands.Bot(command_prefix = "?",intents=intents)
+activity = discord.Game(name="!help")
+
+
+
 
 async def send_message(message, user_message, is_private):
 
@@ -31,6 +35,23 @@ async def send_message(message, user_message, is_private):
 def run_discord_bot():
     @bot.event
     async def on_ready():
+        await bot.change_presence(status=discord.Status.idle, activity=activity)
+        for guild in bot.guilds:
+            if 'Mafia' not in [role.name for role in guild.roles]:
+                await guild.create_role(name='Mafia')
+            role = discord.utils.get(guild.roles, name='Mafia')
+            found = []
+            for channel in guild.channels:
+                if channel.name == 'mafia' or channel.name == 'Mafia':
+                    found.append(channel.type)
+            if discord.ChannelType.text not in found:
+                await guild.create_text_channel('mafia')
+            if discord.ChannelType.voice not in found:
+                await guild.create_voice_channel('Mafia')
+            for channel in guild.channels:
+                if channel.name == 'mafia' or channel.name == 'Mafia':
+                    await channel.set_permissions(guild.default_role, read_messages = False)
+                    await channel.set_permissions(role, read_messages = True)
         print("bot is ready !")
 
     @bot.event
